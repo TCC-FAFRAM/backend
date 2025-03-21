7b3e7083f8c22d7c40d91cf55b9fd788466cd7cf
Thiago Monteiro <thiago.monteiro.marcolino@gmail.com>
2025-03-21 20:46:05 -0300

    terminada a trigger de Log

--- backend/prisma/sql/trigger_log.sql
+++ backend/prisma/sql/trigger_log.sql
@@ -1,6 +1,4 @@
--- NAO ESTA PRONTO
-
--- funcao da trigger de log
+-- Criar a função de log
 CREATE OR REPLACE FUNCTION log_trigger() RETURNS TRIGGER AS $$
 BEGIN
   INSERT INTO "Log" (entidade, acao, dados_anteriores, dados_novos, data_acao)
@@ -15,17 +13,20 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- triggers para as tabelas Usuario, Funcao e Curso
--- cria novas triggers e apaga as antigas se existir
+-- cria triiger de log de usuario, funcao e curso
 DO $$ 
 DECLARE r RECORD;
 BEGIN
-  FOR r IN SELECT tablename FROM pg_tables WHERE schemaname = 'public' AND tablename IN ('usuario', 'funcao', 'curso') LOOP
-    EXECUTE format('DROP TRIGGER IF EXISTS log_trigger ON "%s"', r.tablename);
+  FOR r IN SELECT tablename FROM pg_tables WHERE schemaname = 'public' 
+           AND tablename IN ('Usuario', 'Funcao', 'Curso') LOOP
+    -- Removendo a trigger antiga, se existir
+    EXECUTE format('DROP TRIGGER IF EXISTS log_trigger ON %I', r.tablename);
+    
+    -- Criando a nova trigger
     EXECUTE format(
-      'CREATE TRIGGER log_trigger AFTER INSERT OR UPDATE OR DELETE ON "%s" FOR EACH ROW EXECUTE FUNCTION log_trigger()',
+      'CREATE TRIGGER log_trigger AFTER INSERT OR UPDATE OR DELETE ON %I 
+       FOR EACH ROW EXECUTE FUNCTION log_trigger()', 
       r.tablename
     );
   END LOOP;
 END $$;
-
