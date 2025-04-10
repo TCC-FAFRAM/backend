6c7a51eb0f65515c2a1cfe7aad5709d917d2fd1e
caique-radam <caiqueradam@hotmail.com>
2025-04-10 20:25:49 -0300

    fix: remove validações do cpf

--- backend/src/services/UsuarioService.ts
+++ backend/src/services/UsuarioService.ts
@@ -1,7 +1,6 @@
 import { Usuario } from "@prisma/client";
-import { BaseService, IBaseService, PagedResult } from "../bases/BaseService";
+import { BaseService, IBaseService } from "../bases/BaseService";
 import { UsuarioRepository } from "../models/UsuarioRepository";
-import { maskCPF, validarCPF } from "../utils/FuncsUtils";
 
 interface IUsuarioService extends IBaseService<Usuario> {
   findByEmail(userEmail: string): Promise<Usuario | null>
@@ -10,36 +9,6 @@ interface IUsuarioService extends IBaseService<Usuario> {
 export class UsuarioService extends BaseService<Usuario> implements IUsuarioService {
   constructor() {
     super(new UsuarioRepository());
-  }
-
-  async create(data: Usuario): Promise<Usuario> {
-    if (!validarCPF(data.cpf)) {
-      throw new Error('Invalid CPF');
-    }
-    return super.create(data);
-  }
-
-  async getAll(params?: any): Promise<PagedResult<Usuario>> {
-    const result = await super.getAll(params);
-    if (result.data) {
-      result.data = result.data.map(user => ({
-        ...user,
-        cpf: maskCPF(user.cpf)
-      }));
-    }
-    return result;
-  }
-
-  async getById(id: number): Promise<Usuario | null> {
-    const user = await super.getById(id);
-
-    if (user) {
-      const maskedUser = { ...user };
-      maskedUser.cpf = maskCPF(user.cpf);
-      return maskedUser;
-    }
-
-    return null;
   }
 
   async findByEmail(userEmail: string): Promise<Usuario | null> {
